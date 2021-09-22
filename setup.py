37f0eb0d682344281e114cb8a0aaf1242a08b75d
Abhendu21 <abhendu.chowdhury1@three.co.uk>
2021-09-22 16:02:37 +0100

    2nd commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from cicd_demo_v2 import __version__
+
+setup(
+    name="cicd_demo_v3",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=["wheel"],
+    version=__version__,
+    description="Databricks Labs CICD Templates Sample Project",
+    author="",
+)
